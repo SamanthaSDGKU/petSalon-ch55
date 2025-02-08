7f966de3945a8c075a5f227d5ce2a59328a56190
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2025-02-08 14:18:11 -0800

    delete function

---
+++ scripts/display.js
@@ -0,0 +1,48 @@
+function displayPet(){
+    let cardSection = document.getElementById("petNames");
+    let result = "";
+
+    for(let i=0; i<petList.length;i++){
+        //console.log(petList[i]);
+        let pet = petList[i];
+        result += `
+            <div id=${i} class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Name: ${pet.name}</h5>
+                    <h6 class="card-subtitle mb-2 text-body-secondary">Age: ${pet.age}</h6>
+                    <p class="card-text">Gender: ${pet.gender}</p>
+                    <p class="card-text">Breed: ${pet.breed}</p>
+                    <p class="card-text">Service: ${pet.service}</p>
+                </div>
+            </div>
+        ` 
+    }
+    
+    cardSection.innerHTML = result;
+}
+function displayPetsInfo(){
+    document.getElementById("petsCounter").innerHTML= "Total pets = " + petList.length;
+}
+function displayTable(){
+    let petRows = document.getElementById("petRows");
+    let rows="";
+    for(let i=0; i<petList.length;i++){
+        let pet = petList[i];
+        rows +=`
+        <tr id=${i}>
+            <td>${pet.name}</td>
+            <td>${pet.age}</td>
+            <td>${pet.gender}</td>
+            <td>${pet.breed}</td>
+            <td>${pet.service}</td>
+            <td>
+                <button class="btn btn-danger btn-sm" onclick="deletePet(${i})">Delete</button>
+                <button class="btn btn-info btn-sm disabled">Edit</button>
+            </td>
+        </tr>
+        `;
+    }
+    petRows.innerHTML=rows;
+}
+
+// go to the script.js and replace displayPet() by displayTable()
--- scripts/script.js
+++ scripts/script.js
@@ -11,36 +11,100 @@ let inputName=document.getElementById("txtName");
 let inputAge = document.getElementById("txtAge");
 let inputGender = document.getElementById("txtGender");
 let inputBreed = document.getElementById("txtBreed");
+let inputService = document.getElementById("txtService");
 //constructor 
 //name, age, gender, breed,
-function Pet(name,age,gender,breed){
+function Pet(name,age,gender,breed,service){
     this.name=name;
     this.age=age;
     this.gender=gender;
     this.breed=breed;
+    this.service=service;
 }
 function register(){ 
     // get the values from inputs
     // create obj using contructor
-    let newPet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value);
+    let newPet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value);
     console.log(newPet);
     //push the obj into the array
-    petList.push(newPet);
-    clearInputs();
+    if(isValid(newPet)){
+        petList.push(newPet);
+        displayTable();
+        displayPetsInfo();
+        clearInputs();
+        showAlert("A new pet was added","success");
+    }else{
+        showAlert("Please, add all  the information","danger");
+    }
 }
+
+//Validations
+function isValid(pet){
+    let validation = true;
+
+    //reset the style
+    inputName.classList.remove("error");
+    inputAge.classList.remove("error");
+    document.getElementById("alert-error").classList.add("hide");
+
+    if(pet.name === ""){
+        validation = false;
+        inputName.classList.add("error");
+    }
+    if(pet.age === ""){
+        validation = false;
+        inputAge.classList.add("error");
+    }
+    return validation;
+}
+
+
 function clearInputs(){
     inputName.value="";
     inputAge.value="";
     inputGender.value="";
     inputBreed.value="";
+    inputService.value="";
+}
+
+function deletePet(petId){
+    //remove from HTML
+    document.getElementById(petId).remove();
+
+    // remove from the array splice()
+    petList.splice(petId,1);
+
+    //re-display the table
+    displayTable();
+    displayPetsInfo();
+
+    //show the notification
+    showAlert("The pet was deleted","warning");
+}
+
+function showAlert(msg,type){
+    let alertContainer = document.getElementById("alertContainer");
+
+    alertContainer.innerHTML=`
+        <div id="alert-error" class="alert alert-${type}" role="alert">
+            ${msg}
+        </div>
+    `;
+
+    setTimeout(()=>{
+        document.getElementById("alert-error").remove();
+    },3000);// 1000 = 1s
 }
 
 function init(){
     // create three pets
-    let pet1 = new Pet("Scooby",65,"Male","Dane");
-    let pet2 = new Pet("Scrappy",55,"Male","Mixed");
+    let pet1 = new Pet("Scooby",65,"Male","Dane","Grooming");
+    let pet2 = new Pet("Scrappy",55,"Male","Mixed","Vaccines");
     // push the pets into the array
     petList.push(pet1,pet2);
+    displayTable();
+    displayPetsInfo();
+    document.getElementById("alert-error").classList.add("hide");
 }
 
 window.onload=init;// wait to render the HTML
